7f72eda0a230d290ecf89d2a875b622e9d653937
Mariam Araqelyan <mariam-araqelyan91@mail.ru>
2021-03-29 20:15:50 +0400

    initial commit

---
+++ src/components/TVChartContainer/api/pairs.js
@@ -0,0 +1,97 @@
+export default {
+    "ETH/USDT": "BINANCE",
+    "BNB/BKRW": "BINANCE",
+    "PHB/TUSD": "BINANCE",
+    "BNB/AUD": "BINANCE",
+    "BNB/GBP": "BINANCE",
+    "BNB/EUR": "BINANCE",
+}
+
+export const data = [
+    {
+        symbol: 'ETH/USDT',
+        full_name: 'ETH/USDT',
+        description: 'Ethereum / TetherUS',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    },
+    {
+        symbol: 'BNB/BKRW',
+        full_name: 'BNB/BKRW',
+        description: 'Binance KRW',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    },
+    {
+        symbol: 'PHB/TUSD',
+        full_name: 'PHB/TUSD',
+        description: 'Red Pulse Phoenix Binance',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    },
+    {
+        symbol: 'BNB/AUD',
+        full_name: 'BNB/AUD',
+        description: 'Binance Coin / AUD',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    },
+    {
+        symbol: 'BNB/GBP',
+        full_name: 'BNB/GBP',
+        description: 'Binance Coin / GBP',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    },
+    {
+        symbol: 'BNB/EUR',
+        full_name: 'BNB/EUR',
+        description: 'Binance Coin / EUR',
+        exchange: 'BINANCE',
+        type: 'crypto',
+        provider_id: "binance"
+    }
+]
+
+// export const data = [
+// 	{
+// 		symbol: 'BTC/USD',
+// 		full_name: 'BTC/USD',
+// 		description: 'description',
+// 		exchange: 'Coinbase',
+// 		type: 'type'
+// 	},
+// 	{
+// 		symbol: 'ETH/USD',
+// 		full_name: 'ETH/USD',
+// 		description: 'description',
+// 		exchange: 'Coinbase',
+// 		type: 'type'
+// 	},
+// 	{
+// 		symbol: 'XLM/BTC',
+// 		full_name: 'XLM/BTC',
+// 		description: 'description',
+// 		exchange: 'Bitfinex',
+// 		type: 'type'
+// 	},
+// 	{
+// 		symbol: 'XLM/ETH',
+// 		full_name: 'XLM/ETH',
+// 		description: 'description',
+// 		exchange: 'Bitfinex',
+// 		type: 'type'
+// 	},
+// 	{
+// 		symbol: 'XLM/USD',
+// 		full_name: 'XLM/USD',
+// 		description: 'description',
+// 		exchange: 'Bitfinex',
+// 		type: 'type'
+// 	}
+// ]
